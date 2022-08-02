efb0c7b8c2bfb00c0215354c241f5e983874d65b
Chanwit Kaewkasi <chanwit@gmail.com>
2022-08-03 02:48:30 +0700

    add helm chart

--- main.ts
+++ main.ts
@@ -1,61 +1,9 @@
-import { Construct } from 'constructs';
-import { App, TerraformStack } from 'cdktf';
+import { App } from 'cdktf';
 
-import * as kubernetes from '@cdktf/provider-kubernetes'
+import { AppStack } from './app'
 import { BootstrapStack } from './bootstrap'
 
-
-class MyStack extends TerraformStack {
-
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
-    
-    new kubernetes.KubernetesProvider(this, 'k8s', {
-    })
-
-    new kubernetes.Deployment(this, 'deployment_myapp', {
-      metadata: {
-        labels: {
-          app: 'myapp',
-          component: 'frontend',
-          environment: 'dev',
-        },
-        name: 'myapp',
-        namespace: 'dev',
-      },
-      spec: {
-        replicas: '2',
-        selector: {
-          matchLabels: {
-            app: 'myapp',
-            component: 'frontend',
-            environment: 'dev',
-          },
-        },
-        template: {
-          metadata: {
-            labels: {
-              app: 'myapp',
-              component: 'frontend',
-              environment: 'dev',
-            },
-          },
-          spec: {
-            container: [
-              {
-                image: 'nginx:latest',
-                name: 'frontend',
-              },
-            ],
-          },
-        },
-      },
-    })
-
-  }
-}
-
 const app = new App();
-new MyStack(app, 'app');
+new AppStack(app, 'app');
 new BootstrapStack(app, 'bootstrap')
 app.synth();
